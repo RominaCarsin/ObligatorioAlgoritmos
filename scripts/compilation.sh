bf86fd5e1dcdb8bc4571ce348f89c649c9e4f673
luchosangui <luchosangui20@gmail.com>
2025-09-08 22:07:55 -0300

    upload with all tests adn scripts to be deleted for final version

---
+++ scripts/compilation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#set -x
+BUILD_FOLDER=./build
+FOLDER=./
+
+mkdir -p "$BUILD_FOLDER" 
+
+TEST_FOLDER=./tests/ejercicio1
+CPPFILE=$FOLDER/ejercicio1.cpp
+
+
+echo "Compilando..."
+if [ -f "$CPPFILE" ]; then
+    echo "Realizado en C++"
+    if ! g++ $CPPFILE -o $BUILD_FOLDER/ejercicio1.out --std=c++11; then
+    echo -e "\e[31mERROR en compilacion\e[0m"
+    exit 1
+    fi
+fi
+echo "Compilado terminado" 
