cf59c1c1a15691b8d02e6e02480b90f04f985d3b
Romina Carsin <romicarsin@gmail.com>
2025-11-06 19:35:55 -0300

    ejercicio 6 con errores es ultimas pruebas

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -2,11 +2,100 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/Greedy.cpp"
 
 using namespace std;
 
+struct Par
+{
+    int primero;
+    int segundo;
+};
+
 int main()
 {
-    // TODO
-    return 0;
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    int f;
+    cin >> f;
+
+    Par pozos[n + 1];
+    Par mejoras[m + 1];
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        int pozoInicio;
+        cin >> pozoInicio;
+
+        int pozoFin;
+        cin >> pozoFin;
+
+        pozos[i].primero = pozoInicio;
+        pozos[i].segundo = pozoFin;
+    }
+
+    for (int j = 1; j <= m; j++)
+    {
+
+        int casaMejora;
+        cin >> casaMejora;
+
+        int potencia;
+        cin >> potencia;
+
+        mejoras[j].primero = casaMejora;
+        mejoras[j].segundo = potencia;
+    }
+
+    int cantMejoras = 0;
+    int posActual = 1;
+    int potenciaActual = 1;
+    int posPozo = 1;
+    int posMejora = 1;
+
+    maxHeap heap(m);
+
+    while (posActual < f)
+    {
+        while (posMejora <= m && mejoras[posMejora].primero <= posActual + potenciaActual)
+        {
+            heap.insertar(mejoras[posMejora].segundo);
+            posMejora++;
+        }
+
+        // Hay un pozo, puedo saltarlo?
+        if (posPozo <= n && pozos[posPozo].primero <= posActual + potenciaActual)
+        {
+            // Sí, sigo de largo
+            if (pozos[posPozo].segundo < posActual + potenciaActual)
+            {
+                posActual = max(posActual + potenciaActual, pozos[posPozo].segundo + 1);
+                posPozo++;
+            }
+            // No, necesito una mejora
+            else
+            {
+                if (heap.estaVacio())
+                {
+                    cout << "Imposible" << endl;
+                    return 0;
+                }
+
+                // saco del minHeap y uso la mejora
+                potenciaActual += heap.extraerMax();
+                heap.eliminar();
+                cantMejoras++;
+            }
+        }
+        // No hay pozo, avanzo
+        else
+        {
+            posActual += potenciaActual;
+        }
+    }
+    cout << cantMejoras << endl;
+    return 0;
 }
--- tads/Greedy.cpp
+++ tads/Greedy.cpp
@@ -1,20 +1,109 @@
-#include <cassert>
-#include <string>
 #include <iostream>
-#include <limits>
-#include "tads/Greedy.cpp"
+#include "../funciones/enteros.cpp"
+#include "List.h"
 
 using namespace std;
 
-struct Greedy
-{
-    int inicio;
-    int fin;
-};
+class maxHeap
+{ // de más grande a más chico (hijos menores al padre)
+private:
+    int capacidad;
+    int cantElementos;
+    int *vector;
 
-struct Mejora
-{
-    int pos;
-    int potencia;
-};
+    bool lleno()
+    {
+        return cantElementos == capacidad;
+    }
 
+    int padre(int pos)
+    {
+        return pos / 2;
+    }
+
+    int hijoIzq(int pos)
+    {
+        return pos * 2;
+    }
+
+    int hijoDer(int pos)
+    {
+        return pos * 2 + 1;
+    }
+
+    void intercambiar(int posPadre, int posHijo)
+    {
+        int aux = vector[posPadre];
+        vector[posPadre] = vector[posHijo];
+        vector[posHijo] = aux;
+    }
+
+    bool comparar(int posPadre, int posHijo)
+    {
+        return vector[posPadre] < vector[posHijo];
+    }
+
+    void flotar(int pos)
+    { // balancea comparando a los hijos con el padre
+        if (pos > 1)
+        {
+            int posPadre = padre(pos);
+            if (comparar(posPadre, pos))
+            {
+                intercambiar(posPadre, pos);
+                flotar(posPadre);
+            }
+        }
+    }
+
+    void hundir(int pos)
+    { // balancea comparando a el padre con los hijos
+        int hijoI = hijoIzq(pos);
+        int hijoD = hijoDer(pos);
+
+        if (hijoI > cantElementos)
+            return; // no tiene hijos
+
+        int mayorHijo = hijoI;
+        if (hijoD <= cantElementos && comparar(mayorHijo, hijoD))
+        {
+            mayorHijo = hijoD;
+        }
+        if (comparar(pos, mayorHijo))
+        {
+            intercambiar(pos, mayorHijo);
+            hundir(mayorHijo);
+        }
+    }
+
+public:
+    bool estaVacio()
+    {
+        return cantElementos == 0;
+    }
+
+    maxHeap(int c)
+    {
+        capacidad = c;
+        cantElementos = 0;
+        vector = new int [c + 2];
+    }
+
+    void insertar(int valor){
+        if (lleno()) return;
+            vector[++cantElementos] = valor;
+            flotar(cantElementos);
+    }
+
+    int extraerMax(){
+        if (estaVacio()) return -1;
+        return vector[1];
+    }
+
+    void eliminar()
+    {
+        if (estaVacio()) return;
+        vector[1] = vector[cantElementos--];
+        hundir(1);
+    }
+};
