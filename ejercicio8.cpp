976f6edf7755102e14a87408b855f2aab7f2d970
Luis Sanguinetti <luis.eduardo.sanguinetti@gmail.com>
2025-11-17 16:29:26 -0300

    esquema

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -5,8 +5,43 @@
 
 using namespace std;
 
-int main()
-{
-    // TODO
+const int MAXN = 1000;
+int cris[MAXN];
+
+int maxPoints(int leftIndex, int rightIndex, int k /*amount of elements in subarray*/, int n /*total elements*/) {
+    // ya recorri todo
+    if(l>r) return 0;
+    int val = getValue(l,r,k);
+    
+    //recorro el array 1 menos de la der y hago qeue el valor sea k^2 + el siguiente de puntos
+    int res = maxPoints(l,r-1,0,n)+((k+1)*(k+1));
+    //  cuando salgo del recursivo estoy en lo mas abajo
+    //calculo valor por elementos del mismo color
+    for(int i=0;i<r;i++){
+        if(cris[i]==cris[r]){
+            int cand = maxPoints(l,i,k+1,n)+ maxPoints(i+1,r-1,0,n);
+            if(cand>res) res=cand;
+        }
+    }
+    // el nuevo mejor valor para esa posicion
+    setValue(l,r,k,res);
+    return res;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> cris[i];
+    }
+    
+    // tad base case fill por ejemplo en array es array = -1
+    setUp();
+
+    int ans = maxPoints(0, n - 1, 0, n);// recorro del mas grande 
+    cout << ans << endl;
+    
+    cleanup();
     return 0;
 }
--- scripts/compilation.sh
+++ scripts/compilation.sh
@@ -7,14 +7,14 @@ FOLDER=./
 
 mkdir -p "$BUILD_FOLDER" 
 
-TEST_FOLDER=./tests/ejercicio6
-CPPFILE=$FOLDER/ejercicio6.cpp
+TEST_FOLDER=./tests/ejercicio8
+CPPFILE=$FOLDER/ejercicio8.cpp
 
 
 echo "Compilando..."
 if [ -f "$CPPFILE" ]; then
     echo "Realizado en C++"
-    if ! g++ $CPPFILE -o $BUILD_FOLDER/ejercicio6.out --std=c++11; then
+    if ! g++ $CPPFILE -o $BUILD_FOLDER/ejercicio8.out --std=c++11; then
     echo -e "\e[31mERROR en compilacion\e[0m"
     exit 1
     fi
